b40169cdced5eeb0f0cc1f42d9b028c5ed3e23e6
Anaïs Lorteau <anaislor@orange.fr>
2019-09-09 18:58:26 +0200

    css, all plants, one plant

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const user = require("../models/User");
 const plantModel = require("../models/Plant");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 // POSTING the survey responses
-router.get('/survey/:id', (req, res, next) => {
+router.get("/survey/:id", (req, res, next) => {
   user
     .getOne(req.params.id)
     .then(dbRes => {
-      res.render('/users/survey', { user: dbRes });
+      res.render("users/survey", { user: dbRes });
     })
     .catch(error => {
       console.log(error);
@@ -21,9 +21,8 @@ router.get('/survey/:id', (req, res, next) => {
     });
 });
 
-router.post('/survey/:id', (req, res, next) => {
-
-  const {outside,lighting, humidity, animals, type_of_plants} = req.body
+router.post("/survey/:id", (req, res, next) => {
+  const { outside, lighting, humidity, animals, type_of_plants } = req.body;
 
   const newUserTest = {
     outside,
@@ -33,67 +32,64 @@ router.post('/survey/:id', (req, res, next) => {
     type_of_plants
   };
 
-  if(req.body === "both"){
-    req.body === {}
+  if (req.body === "both") {
+    req.body === {};
   }
 
   if (!outside || !lighting || !humidity || !animals || !type_of_plants) {
-    res.render("/survey", {
-      errorMessage: "Please fill all fields, otherwise we can't give you advice!"
+    res.render("survey", {
+      errorMessage:
+        "Please fill all fields, otherwise we can't give you advice!"
     });
     return;
   }
 
   plantModel
-  .find({
-    outside: newUserTest.outside, 
-    lighting: newUserTest.lighting, 
-    humidity: newUserTest.humidity,
-    animals: newUserTest.animals,
-    type_of_plants: newUserTest.type_of_plants 
-  })
-  .then(dbRes => {
-    req.body = {};
-    res.redirect("/profile/:id");
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .find({
+      outside: newUserTest.outside,
+      lighting: newUserTest.lighting,
+      humidity: newUserTest.humidity,
+      animals: newUserTest.animals,
+      type_of_plants: newUserTest.type_of_plants
+    })
+    .then(dbRes => {
+      req.body = {};
+      res.redirect("/profile/:id");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-
 /* GET the user profile page */
-router.get('/profile/:id', (req, res, next) => {
-  user
-  .findById(req.params.id)
-  .then(dbRes => {
-    res.render('/users/user_profile', {user: dbRes});
-  })
+router.get("/profile/:id", (req, res, next) => {
+  user.findById(req.params.id).then(dbRes => {
+    res.render("users/user_profile", { user: dbRes });
+  });
 });
 
 /* GET the page showing all plants*/
-router.get('/plants', (req, res, next) => {
+router.get("/plants", (req, res, next) => {
   plantModel
-  .find()
-  .then(dbRes => {
-    res.render('/plants/all_plants', {plants: dbRes});
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .find()
+    .then(dbRes => {
+      res.render("plants/all_plants", { plants: dbRes });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 /* GET the page showing ONE plant */
-router.get('/plants/:id', (req, res, next) => {
-  
+router.get("/plants/:id", (req, res, next) => {
   plantModel
-  .findById(req.params.id)
-  .then(dbRes => {
-    res.render('/plants/one_plant', {plant: dbRes});
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .findById(req.params.id)
+    .then(dbRes => {
+      res.render("plants/one_plant", { plant: dbRes });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
