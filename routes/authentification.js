b327904b0e46fd202fe46ebd731fe95a3278cebf
Anaïs Lorteau <anaislor@orange.fr>
2019-09-12 14:16:54 +0200

    all plants add to fav, add plants to db

--- routes/authentification.js
+++ routes/authentification.js
@@ -1,40 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const UserModel = require('./../models/User');
-const cloudinary = require('./../config/cloudinary');
+const bcrypt = require("bcrypt");
+const UserModel = require("./../models/User");
+const cloudinary = require("./../config/cloudinary");
 
-router.get('/signup', (req, res) => {
-	res.render('users/signup');
+router.get("/signup", (req, res) => {
+  res.render("users/signup");
 });
 
-router.post('/signup', cloudinary.single('avatar'), (req, res) => {
-	UserModel.findOne({ email: req.body.email })
-		.then((dbRes) => {
-			if (dbRes) {
-				res.render('users/signup', {
-					msg: 'An account already exists with this email'
-				});
-				return;
-			} else {
-				const { name, email, password } = req.body;
-				const newUser = {
-					name,
-					email,
-					password
-				};
-				const salt = bcrypt.genSaltSync(10);
-				const hashed = bcrypt.hashSync(newUser.password, salt);
-				newUser.password = hashed;
+router.post("/signup", cloudinary.single("avatar"), (req, res) => {
+  UserModel.findOne({ email: req.body.email })
+    .then(dbRes => {
+      if (dbRes) {
+        res.render("users/signup", {
+          msg: "An account already exists with this email"
+        });
+        return;
+      } else {
+        const { name, email, password } = req.body;
+        const newUser = {
+          name,
+          email,
+          password
+        };
+        const salt = bcrypt.genSaltSync(10);
+        const hashed = bcrypt.hashSync(newUser.password, salt);
+        newUser.password = hashed;
 
-				if (req.file) {
-					newUser.avatar = req.file.secure_url;
-				}
+        if (req.file) {
+          newUser.avatar = req.file.secure_url;
+        }
 
         UserModel.create(newUser)
           .then(response => {
             req.session.currentUser = newUser;
-            res.redirect(`/profile`);
+            res.redirect(`/plants`);
           })
           .catch(error => console.log(error));
       }
@@ -42,35 +42,43 @@ router.post('/signup', cloudinary.single('avatar'), (req, res) => {
     .catch(dberr => console.log(dberr));
 });
 
-router.get('/login', (req, res) => {
-	res.render('users/login');
+router.get("/login", (req, res) => {
+  res.render("users/login");
 });
 
-router.post('/login', (req, res) => {
-	const user = req.body;
-	UserModel.findOne({ email: user.email })
-		.then((dbRes) => {
-			if (!dbRes) {
-				res.render('users/login', { msg: 'Incorrect email or password' });
-				return;
-			} else {
-				if (bcrypt.compareSync(user.password, dbRes.password)) {
-					req.session.currentUser = user;
-					res.redirect(`/`);
-				} else {
-					res.render('users/login', { msg: 'Incorrect email or password' });
-					return;
-				}
-			}
-		})
-		.catch((dberr) => console.log(dberr));
+router.post("/login", (req, res) => {
+  const user = req.body;
+  UserModel.findOne({ email: user.email })
+    .then(dbRes => {
+      if (!dbRes) {
+        res.render("users/login", { msg: "Incorrect email or password" });
+        return;
+      } else {
+        if (bcrypt.compareSync(user.password, dbRes.password)) {
+          req.session.currentUser = user;
+          res.redirect(`/plants`);
+        } else {
+          res.render("users/login", { msg: "Incorrect email or password" });
+          return;
+        }
+      }
+    })
+    .catch(dberr => console.log(dberr));
 });
 
-router.get('/logout', (req, res) => {
-	req.session.destroy((err) => {
-		res.locals.loggedin = 'false';
-		res.redirect('/');
-	});
+router.get("/logout", (req, res) => {
+  req.session.destroy(err => {
+    res.locals.loggedin = "false";
+    res.redirect("/");
+  });
+});
+
+router.post("/user", (req, res) => {
+  if (req.session.currentUser) {
+    res.send(true);
+  } else {
+    res.send(false);
+  }
 });
 
 module.exports = router;
